658845cbf6094320e93b16288581a585d70c5850
Tommie Jones <atlantageek@gmail.com>
2017-09-27 00:55:11 -0400

    Adding files

---
+++ data.js
@@ -0,0 +1,335 @@
+var data = {
+    "name": "root",
+    "children": [
+      {
+         name: "Region A",
+         children: [
+           {
+             name: "District AA",
+             children: [
+               {
+                 name: "Location AAA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location AAB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location AAC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location AAD",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location AAE",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location AAF",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District AB",
+             children: [
+               {
+                 name: "Location ABA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ABB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ABC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ABD",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ABE",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ABF",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District AC",
+             children: [
+               {
+                 name: "Location ACA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ACB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location ACC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+             ]
+           }
+         ]
+      } ,
+      {
+         name: "Region B",
+         children: [
+           {
+             name: "District BA",
+             children: [
+               {
+                 name: "Location BAA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BAB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BAC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BAD",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BAE",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BAF",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District BB",
+             children: [
+               {
+                 name: "Location BBA",
+                 neutral: 1,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BBB",
+                 neutral: 2,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BBC",
+                 neutral: 3,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BBD",
+                 neutral: 4,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BBE",
+                 neutral: 5,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BBF",
+                 neutral:6,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District AC",
+             children: [
+               {
+                 name: "Location BCA",
+                 neutral: 9,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BCB",
+                 neutral: 8,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location BCC",
+                 neutral: 7,
+                 promoter: 12,
+                 detractor: 5
+               },
+             ]
+           }
+         ]
+      } ,
+      {
+         name: "Region C",
+         children: [
+           {
+             name: "District CA",
+             children: [
+               {
+                 name: "Location CAA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CAB",
+                 neutral: 11,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CAC",
+                 neutral: 12,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CAD",
+                 neutral: 13,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CAE",
+                 neutral: 14,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CAF",
+                 neutral: 15,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District CB",
+             children: [
+               {
+                 name: "Location CBA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CBB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CBC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CBD",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CBE",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CBF",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               }
+             ]
+           },
+           {
+             name: "District CC",
+             children: [
+               {
+                 name: "Location CCA",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CCB",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+               {
+                 name: "Location CCC",
+                 neutral: 10,
+                 promoter: 12,
+                 detractor: 5
+               },
+             ]
+           }
+         ]
+      }
+    ]
+  }
